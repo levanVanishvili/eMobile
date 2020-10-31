139fad22bf255ca8619e1c059d06fd9513840b07
levani vanishvili <levan.vanishvili@gmail.com>
2020-11-01 02:45:18 +0400

    Add Operating System To DB

--- eMobile/Data/ApplicationDbContext.cs
+++ eMobile/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using eMobile.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace eMobile.Data
             : base(options)
         {
         }
+        public DbSet<OpSystem> OperatingSystems { get; set; }
     }
 }
---
+++ eMobile/Models/OpSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMobile.Models
+{
+    public class OpSystem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Operating System Name")] 
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}

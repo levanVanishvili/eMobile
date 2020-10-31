d1eb3b9b3faf02a97e99b7aa31634d63e80e7514
levani vanishvili <levan.vanishvili@gmail.com>
2020-11-01 00:36:47 +0400

    Creatin Adin and Customer Areas

--- eMobile/Areas/Customer/Controllers/HomeController.cs
+++ eMobile/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using eMobile.Models;
 
-namespace eMobile.Controllers
+namespace eMobile.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
